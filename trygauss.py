2a306e8982415cea08f8ced2c694d133db2e8735
cikgubaz <107390299+cikgubaz@users.noreply.github.com>
2022-06-13 15:48:38 +0800

    Add files via upload

---
+++ trygauss.py
@@ -0,0 +1,25 @@
+#!/user/bin/env python
+
+import random
+
+def main():
+    """"Start a number guessing game between 1 - 100."""
+    print("Guess the number!")
+
+    x = random.gauss(1, 2)
+    print(x)
+    guess = None
+
+    while x != guess:
+
+        guess = int(input("Pick a number between 1 to 100: "))
+
+        if x == guess:
+            print("You genius!")
+        elif x > guess:
+            print("Try a bigger number.")
+        elif x < guess:
+            print("Try a smaller number.")
+
+
+main()
