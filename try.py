1a719d01bf6421a8635eac3a365fed70dec068a5
cikgubaz <107390299+cikgubaz@users.noreply.github.com>
2022-06-13 15:48:21 +0800

    Add files via upload

---
+++ try.py
@@ -0,0 +1,23 @@
+#!/user/bin/env python
+
+import random
+
+def main():
+    """"Start a number guessing game between 1 - 100."""
+    print("Guess the number!")
+
+    x = random.randint(1, 100)
+    guess = None
+
+    while x != guess:
+
+        guess = int(input("Pick a number between 1 to 100: "))
+
+        if x == guess:
+            print("You cheat!")
+        else:
+            print("hihihihi....")
+        
+
+
+main()
